2b8f6a8c06e819bad4b6a311beb843d53abb5b2c
eladgura <Eladgura2@gmail.com>
2024-07-08 14:35:33 +0300

    .the 9 commit

--- app.py
+++ app.py
@@ -1,34 +1,8 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
+from app import create_app, db
 from flask_cors import CORS
-
-# Initialize Flask app
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialize extensions
-db = SQLAlchemy(app)
+app = create_app()
 CORS(app)
-
-# Define a simple model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    
-    def __repr__(self):
-        return f'<User {self.name}>'
-
-# Define a route
 @app.route("/<name>")
 def hello(name):
     return f"Hello, {(name)}!"
-
-# Create database tables
-with app.app_context():
-    db.create_all()
-
-# Run the Flask app
-if __name__ == "__main__":
-    app.run(debug=True)
