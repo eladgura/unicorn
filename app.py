fc88d62a8b9143ca7a280c41a2702ebcfd17bbe3
eladgura <Eladgura2@gmail.com>
2024-07-08 14:41:16 +0300

    .the 9 commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-
-from app import create_app, db
+from app import create_app
 from flask_cors import CORS
+
 app = create_app()
 CORS(app)
+
 @app.route("/<name>")
 def hello(name):
-    return f"Hello, {(name)}!"
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
